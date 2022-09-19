43160ea27ab593f78c7040af743f60345df8e292
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-09-20 03:19:48 +0530

    added CarNavigation.kt and CarController.kt

---
+++ app/src/main/java/com/softaai/roboticcarnavigation/CarController.kt
@@ -0,0 +1,13 @@
+package com.softaai.roboticcarnavigation
+
+
+/**
+ * Created by amoljp19 on 9/20/2022.
+ * softAai Apps.
+ */
+object CarController {
+
+    fun proccess(commandString: String){
+        //ToDo implementation remainging
+    }
+}
---
+++ app/src/main/java/com/softaai/roboticcarnavigation/CarNavigation.kt
@@ -0,0 +1,12 @@
+package com.softaai.roboticcarnavigation
+
+
+/**
+ * Created by amoljp19 on 9/20/2022.
+ * softAai Apps.
+ */
+
+
+fun main(){
+    println("we will test robotic car navigation soon...!!")
+}
