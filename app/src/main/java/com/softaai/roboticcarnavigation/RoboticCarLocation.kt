29b8fe58362dbd3e3ec103ac6bc8e4e1d6abffb9
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-09-20 05:04:22 +0530

    RoboticCarLocation.kt renamed from RoboticCar

---
+++ app/src/main/java/com/softaai/roboticcarnavigation/RoboticCarLocation.kt
@@ -0,0 +1,13 @@
+package com.softaai.roboticcarnavigation
+
+
+/**
+ * Created by amoljp19 on 9/20/2022.
+ * softAai Apps.
+ */
+
+data class RoboticCarLocation(var position: Position = Position(0,0), var facingDirection: FacingDirection = FacingDirection("N")){
+    override fun toString(): String {
+        return "Robotic Car current location at $position and facing towards $facingDirection"
+    }
+}
